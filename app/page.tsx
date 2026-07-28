9c531f3361705b690aaa480ad576dd188505178f
Matthew <8951670+tthmatt@users.noreply.github.com>
2026-07-28 18:22:06 +0800

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { StreamConsole } from "@/components/stream-console";
+
+export const dynamic = "force-dynamic";
+
+export default function Home() {
+  const rtmpBaseUrl =
+    process.env.NEXT_PUBLIC_RTMP_BASE_URL ??
+    "rtmp://ingest.flaredynamics.com:1935/live";
+  const hlsStreamUrl = process.env.NEXT_PUBLIC_HLS_STREAM_URL ?? "";
+
+  return (
+    <StreamConsole
+      rtmpBaseUrl={rtmpBaseUrl}
+      hlsStreamUrl={hlsStreamUrl}
+      streamName="drone"
+    />
+  );
+}
+
